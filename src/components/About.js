50be694178ac1479858f7ad069795f93fab5c866
Cat <morancathy@gmail.com>
2021-10-04 16:59:28 -0500

    created About component

---
+++ src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function About(props) {
+
+  return (
+    <div className="about-me">
+      <img src="/img/Kili.JPG" alt="summitting Kilimanjaro"/>
+      <div className="about-me-text">
+        <h2>Cathy Moran...</h2>
+        <p className="first-p">always reaching for the sky,</p>
+        <p>I am a <strong>software developer</strong> who goes above and beyond. I love immersing myself with code, whether it's building an API or a front-end.</p>
+        <p>A natural multi-tasker, I enjoy solving problems and thrive in fast-paced environments.</p>
+      </div>
+    </div>
+  )
+};
