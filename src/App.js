871c5c94716485f66e26bd646b338e72f66557d2
Cat <morancathy@gmail.com>
2021-10-04 16:57:07 -0500

    created main App page

--- src/App.js
+++ src/App.js
@@ -1,42 +1,33 @@
+import Nav from './components/Nav';
+import About from './components/About';
+import Skills from './components/Skills';
+import Showcase from './components/Showcase';
+// import Resume from './components/Resume';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <div class="header">
-        <header>
-          <h1 class="title">Cathy's Portfolio</h1>
-        </header>
+      <Nav />
+      <div className="main">
+        <About />
+        <Skills />
+        <Showcase />
+        {/*<Resume />*/}
       </div>
-
-      <nav class="nav">
-        <p>Contact</p>
-        <p>Skills</p>
-        <p>Projects</p>
-        <p>Resume</p>
-      </nav>
-
-      <div class="main">
-        <div class="about-me">
-          <h2>About Me / Contact Info</h2>
-        </div>
-        <div class="skills">
-          <h2>Skills</h2>
-        </div>
-        <div class="projects">
-          <h2>Project Showcase</h2>
-        </div>
-        <div class="resume">
-          <h2>Link to resume or pdf</h2>
-        </div>
-      </div>
-
-      <div class="footer">
-        <footer>This is the footer</footer>
-      </div>
-      </header>
+      <Footer />
     </div>
+
   );
 }
 
 export default App;
+
+// <div className="App">
+//   <header className="App-header">
+//   <div class="header">
+//     <header>
+//       <h1 class="title">Cathy's Portfolio</h1>
+//     </header>
+//   </div>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
 import App from './App';
+// import 'bootstrap/scss/bootstrap.scss';
 
 ReactDOM.render(
   <React.StrictMode>
