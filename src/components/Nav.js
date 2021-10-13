81c096d42de260846a5f39ac4f59e4434d7e968a
Cat <morancathy@gmail.com>
2021-10-13 17:07:26 -0500

    updated resume link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -24,7 +24,7 @@ export default function Nav(props) {
           <img src="/img/email.png" alt="email"/>
           <p>contact</p>
         </a>
-        <a className="link pdf" href='https://docs.google.com/document/d/1Qw0vjd9SDmVJQ1kxXa_XbPEBZSS47vO-s_8guvb_dIM/edit?usp=sharing' target="_blank" rel="noreferrer">
+        <a className="link pdf" href='https://drive.google.com/file/d/1Pr5iR3McvkFdeQRvNa7vXL2klE9xmyO6/view?usp=sharing' target="_blank" rel="noreferrer">
           <img src="/img/pdf.png" alt="resume"/>
           <p>resume</p>
         </a>
